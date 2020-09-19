b06c06a25ac2bafa4d6263a81d2e55cf9cdee923
Gerdeth <gerdethwickham@gmail.com>
2020-09-19 18:57:28 +0000

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,31 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+function calculateTip() {
+  var billAmt = document.getElementById("Bill Amount").value;
+  var serviceQuality = document.getElementById("service qual").value;
+  var numberOfPeople = document.getElementById("people").value;
+  if (billAmt == "" || serviceQuality == 0) {
+    alert("Please enter values");
+    return;
+  }
+  if (numberOfPeople == "" || numberOfPeople <= 1) {
+    numberOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+  var total = (billAmt * serviceQuality) / numberOfPeople;
+  total = Math.round(total * 100) / 100;
+  total = total.toFixed(2);
+
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+  document.getElementById("totalTip").style.display = "none";
+  document.getElementById("each").style.display = "none";
+
+  document.getElementById("caluclate").onclick = function() {
+    calculateTip();
+  };
+}
