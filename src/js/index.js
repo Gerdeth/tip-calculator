41f7c3e19c4aba5420bc775051ae4803b91005ee
Gerdeth <gerdethwickham@gmail.com>
2020-09-22 00:17:56 +0000

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
 /* eslint-disable */
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 function calculateTip() {
